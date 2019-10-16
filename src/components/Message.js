62a9032386ffccfd967421a86490821175872a5f
ahmadghzawi <ahmd.ghzawi@gmail.com>
2019-10-16 08:35:28 +0300

    solved, but one of the tests is broken

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {Component} from "react";
 
-const Message = () => {
+class Message extends Component {
+  render() {
     return (
-        <div>
-            <h3 className="text-center message"></h3>
-        </div>
-    )
+        
+      <div>
+        <h3 className="text-center message">
+          {this.props.isFormValid ? 'Form is Complete!' : 'Form is Incomplete!'}
+        </h3>
+      </div>
+    );
+  }
 }
 
 export default Message;
